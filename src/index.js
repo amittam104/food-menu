d9648f778c99200c0ebcf9c40730e42e00bc8bff
Amit Tambulkar <amittambulkar104@gmail.com>
2024-05-21 13:44:38 +0530

    chore: add css classes in JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,18 +58,24 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  // const style = { color: "red" };
+
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -84,7 +91,9 @@ function Footer() {
   // else alert("we are closed");
 
   return (
-    <footer>{new Date().toLocaleTimeString()} We are currently open.</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We are currently open.
+    </footer>
   );
 }
 
@@ -92,7 +101,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
-      <h2>Pizza</h2>
+      <h3>Pizza</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
